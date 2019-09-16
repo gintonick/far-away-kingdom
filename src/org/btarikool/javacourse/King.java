6ed676c196ebbcee2e8e8e43a8caf67c04019f35
Vadim Semenov <bagum.cemehob@gmail.com>
2019-09-16 08:28:26 +0300

    Initial java code commit

---
+++ src/org/btarikool/javacourse/King.java
@@ -0,0 +1,10 @@
+// King.java
+package org.btarikool.javacourse;
+
+public class King {
+
+    public void sayHello(String name) {
+        System.out.format("Hello! I am the King %s", name);
+    }
+
+}
---
+++ src/org/btarikool/javacourse/Kingdom.java
@@ -0,0 +1,11 @@
+// Kingdom.java
+package org.btarikool.javacourse;
+
+public class Kingdom {
+
+    public static void main(String[] args) {
+        System.out.println("Kingdom of Far Far Away");
+        King king = new King();
+        king.sayHello(args[0]);
+    }
+}
