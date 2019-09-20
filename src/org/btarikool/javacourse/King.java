d0d74ac6ab9e50d0bc14aa8c906487270a66d86e
Vadim Semenov <bagum.cemehob@gmail.com>
2019-09-20 07:38:55 +0300

    Kingdom init commit

--- src/org/btarikool/javacourse/King.java
+++ src/org/btarikool/javacourse/King.java
@@ -4,7 +4,13 @@ package org.btarikool.javacourse;
 public class King {
 
     public void sayHello(String name) {
-        System.out.format("Hello! I am the King %s", name);
+        System.out.format("Hello! I am the King %s\n", name);
+        System.out.println("\n" + Float.MAX_VALUE + "  " + Float.MIN_VALUE);
+        System.out.println("\n" + Double.MAX_VALUE + "  " + Double.MIN_VALUE);
+        char a = '\u0554';
+        int b = a;
+        System.out.println("a = " + a + "; b = " + b);
+
     }
 
 }
