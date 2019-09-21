ef1ab12028d0c996da921532f8e7251118eaa45b
Gintonick <rezu@mail.ru>
2019-09-21 18:05:43 +0300

    my praktikum

--- src/org/btarikool/javacourse/Kingdom.java
+++ src/org/btarikool/javacourse/Kingdom.java
@@ -1,11 +1,39 @@
-// Kingdom.java
 package org.btarikool.javacourse;
 
 public class Kingdom {
 
     public static void main(String[] args) {
-        System.out.println("Kingdom of Far Far Away");
-        King king = new King();
-        king.sayHello(args[0]);
+
+        String firstKnight = "Donald";
+        String secondKnight = "Duck";
+        System.out.println ("welcome, my King");
+
+        Kingdom.Lords Lord = new Kingdom.Lords();
+        Lord.LordsMethod();
+
+        Kingdom.King meeting = new Kingdom.King();
+        meeting.greeting("");
+
+    }
+    static class Lords{
+        public void LordsMethod() {
+            firstLord("My King! You are best of the best!");
+            secondLord("I will protect you!");
+        }
+
+        public void secondLord (String Mouse) {
+            System.out.println (Mouse);
+        }
+
+        public void firstLord (String Mikki) {
+            System.out.println (Mikki);
+        }
+    }
+    public static class King {
+        public void greeting (String name) {
+            System.out.format("Hello! I'm your king %s", name);
+
+        }
+
     }
 }
